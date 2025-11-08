53b43fc581166688f954f6ebd6c70a64ccc23541
Elvis Kerebi <kerebipreye@gmail.com>
2025-11-08 14:48:45 +0200

    updated

--- backend/node_modules/@convex-dev/rate-limiter/dist/component/convex.config.js
+++
@@ -1,3 +0,0 @@
-import { defineComponent } from "convex/server";
-export default defineComponent("rateLimiter");
-//# sourceMappingURL=convex.config.js.map
--- backend/node_modules/@convex-dev/rate-limiter/dist/component/time.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * A simple mutation that returns the current server time.
- * This is used by the useRateLimit hook to calculate clock skew.
- *
- * @returns The current server time (Date.now())
- */
-export declare const getServerTime: import("convex/server").RegisteredMutation<"public", {}, Promise<number>>;
-//# sourceMappingURL=time.d.ts.map
